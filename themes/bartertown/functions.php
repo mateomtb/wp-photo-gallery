ef56ca9c981ecbdd041a66fc014f10c8121b7512
Joe Murphy <joe.murphy@gmail.com>
2014-01-29 13:23:48 -0700

    cleanup

--- themes/bartertown/functions.php
+++ themes/bartertown/functions.php
@@ -10,23 +10,11 @@
 	add_action('wp_enqueue_scripts', 'load_scripts');
 
 	define('THEME_URL', get_template_directory_uri());
-	function add_to_context($data){
-		/* this is where you can add your own data to Timber's context object */
-		$data['qux'] = '';
-		$data['menu'] = new TimberMenu();
-		return $data;
-	}
-
 	function add_to_twig($twig){
 		/* this is where you can add your own fuctions to twig */
 		$twig->addExtension(new Twig_Extension_StringLoader());
 		$twig->addFilter('myfoo', new Twig_Filter_Function('myfoo'));
 		return $twig;
-	}
-
-	function myfoo($text){
-    	$text .= ' bar!';
-    	return $text;
 	}
 
 	function load_scripts(){
