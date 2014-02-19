9efa38cebb3c83b0950ba4256fe89ad42186d7e3
Joe Murphy <joe.murphy@gmail.com>
2014-02-19 16:12:02 -0700

    Get the base script on the page

--- plugins/dfm-wp-omniture/dfm-wp-omniture.php
+++ plugins/dfm-wp-omniture/dfm-wp-omniture.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: DFM - Omniture
+ * Plugin Name: DFM Omniture
  * Plugin URI: 
  * Description: 
  * Version: 0.1
@@ -22,6 +22,14 @@ class DFMOmniture
 
     function __construct()
     {
+        $url = array(
+            'dev' => '//assets.adobedtm.com/891d1ce5446bbce2687a12c1b724985a8b1df83a/satelliteLib-3eed698e57f6ca79e34fb4a932c01f681d287698-staging.js',
+            'prod' => '//assets.adobedtm.com/891d1ce5446bbce2687a12c1b724985a8b1df83a/satelliteLib-3eed698e57f6ca79e34fb4a932c01f681d287698.js'
+        );
+        $environment = 'dev';
+        // wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer );
+        wp_enqueue_script('dtm', $url[$environment], false, '', false);
     }
 
 }
+$DFMOmniture = new DFMOmniture();
