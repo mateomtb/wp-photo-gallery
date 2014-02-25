cd4352faefe3e91e8de3dec03271d1f620de3df1
jboho <bohoj@mac.com>
2014-02-25 13:22:40 -0700

    Fixes issue #43 -- sitemap page

---
+++ sitemap.php
@@ -0,0 +1,285 @@
+<?php 
+    $page_type = 'article';
+    $master_name = 'Site Map';
+    $sub_name = "Site Map";
+?>
+<?php require_once('inc/init.php'); // load in base objects ?>
+<?php require_once('temps/header.php'); // load in header ?>
+<div class="row">
+    <div class="content-well site-map">
+        <h1 id="page-top" class="page-header">
+            Newspaper.com Site Map
+        </h1>
+
+        <ul id="sitemapList">
+            <li class="nav-header"><a href="/home" title="Home">Home</a></li>
+            <li class="nav-header">News</a>
+                <ul class="list">
+                    <li><a href=""><strong>All News</strong></a></li>
+                    <li><a href="/breaking-news" title="Breaking News">Breaking News</a></li>
+                    <li><a href="/obituaries" title="Obituaries">Obituaries</a></li>
+                    <li><a href="/crime-courts" title="Crime">Crime</a></li>
+                    <li><a href="/education" title="Education">Education</a></li>
+                    <li><a href="/health" title="Health">Health</a></li>
+                    <li><a href="/science" title="Science">Science</a></li>
+                    <li><a href="/california-earthquakes" title="Earthquakes">Earthquakes</a></li>
+                    <li><a href="/politics-government" title="Politics">Politics</a></li>
+                    <li><a href="/traffic" title="Traffic">Traffic</a></li>
+                    <li><a href="http://weather.mercurynews.com" title="Weather">Weather</a></li>
+                    <li><a href="/nation-world" title="Nation / World">Nation / World</a></li>
+                    <li><a href="/latest-associated-press" title="AP Headlines">AP Headlines</a></li>
+                    <li><a href="/california" title="California">California News</a></li>
+                    <li><a href="/bay-area-news" title="Bay Area News">Bay Area News</a></li>
+                    <li><a href="/weird-news" title="Weird News">Weird News</a></li>
+                    <li><a href="/corrections" title="Corrections">Corrections</a></li>
+                    <li><a href="/news-special-reports" title="Special Reports">News Special Reports</a>
+                        <ul class="circle">
+                            <li><a href="/california-budget" title="California Budget">California Budget</a></li>
+                            <li><a href="/california-high-speed-rail" title="High-Speed Rail">High-Speed Rail</a></li>
+                            <li><a href="/cost-of-dying" title="Cost of Dying">Cost of Dying</a></li>
+                            <li><a href="/umunhum" title="Umunhum">Mt. Umunhum</a></li>
+                            <li><a href="/loss-of-trust" title="Loss of Trust">Loss of Trust</a></li>
+                            <li><a href="/history" title="Archive photos">Photos from the archive</a></li>
+                            <li><a href="/samesexmarriage" title="Same-sex marriage">Same-sex marriage</a></li>
+                            <li><a href="/bart" title="BART extension to San Jose">BART to San Jose</a></li>
+                            <li><a href="/southbayfootball" title="49ers consider Santa Clara stadium">South Bay football</a></li>
+                            <li><a href="/southbaybaseball" title="Baseball in the South Bay">South Bay baseball</a></li>
+                            <li><a href="/salaries" title="Government Salary Data">Government Salary Data</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+            
+            <li class="nav-header">Business
+                <ul class="list">
+                    <li><a href=""><strong>All Business</strong></a></li>
+                    <li><a href="/venture-capital" title="Venture Capital">Venture Capital</a></li>
+                    <li><a href="/markets" title=" ">Financial Markets</a></li>
+                    <li><a href="/personal-finance" title=" ">Personal Finance</a></li>
+                    <li><a href="/real-estate-news" title="Real Estate">Real Estate</a></li>
+                    <li><a href="/drive" title="Drive / Automotive">Drive / Automotive</a></li>
+                    <li><a href="/green-energy" title="Green Energy">Green Energy</a></li>
+                    <li><a href="/data" title="Data Base">Data Base</a></li>
+                    <li><a href="/business-special-reports" title="Business Special Reports">Business Special Reports</a>
+                        <ul class="circle">
+                            <li><a href="/venture-capital-survey" title="Venture Capital Survey">Venture Capital Survey</a></li>
+                            <li><a href="/sv150" title="SV150">Silicon Valley 150</a></li>
+                            <li><a href="/salary-survey" title="Salary Survey">What the Boss Makes</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="/business-headlines" title="Business Headlines">Business Headlines</a></li>
+                    <li><a href="/breakingbusiness/" title="AP Business News">Business News from the AP</a></li>
+                </ul>
+            </li>
+            
+            <li class="nav-header">Technology
+                <ul class="list">
+                    <li><a href="http://www.siliconvalley.com" title="Technology"><strong>All Technology</strong></a></li>
+                    <li><a href="http://www.siliconvalley.com/news" title="News">News</a></li>
+                    <li><a href="http://www.siliconvalley.com/opinion" title="Opinion">Opinion</a></li>
+                    <li><a href="http://www.siliconvalley.com/specialreports" title="Special Reports">Special Reports</a></li>
+                    <li><a href="http://www.siliconvalley.com/services" title="Services">Services</a></li>
+                </ul>
+            </li>
+            
+            <li class="nav-header">Sports
+                <ul class="list">
+                    <li><a href=""><strong>All Sports</strong></a></li>
+                    <li><a href="/49ers" title="49ers">49ers</a></li>
+                    <li><a href="/raiders" title="Raiders">Raiders</a></li>
+                    <li><a href="/giants" title="Giants">Giants</a></li>
+                    <li><a href="/athletics" title="Athletics">Athletics</a></li>
+                    <li><a href="/sharks" title="Sharks">Sharks</a></li>
+                    <li><a href="/warriors" title="Warriors">Warriors</a></li>
+                    <li><a href="/highschoolsports" title="High School Sports">High School Sports</a></li>
+                    <li><a href="/collegesports" title="College Sports">College Sports</a>
+                        <ul class="circle">
+                            <li><a href="/stanford" title="Stanford">Stanford</a></li>
+                            <li><a href="/cal" title="Cal">Cal</a></li>
+                            <li><a href="/sjsu" title="SJSU">SJSU</a></li>
+                            <li><a href="/santaclara" title="Santa Clara">SCU</a></li>
+                        </ul>
+                    </li
+                    <li><a href="/golf" title="Golf">Golf</a></li>
+                    <li><a href="/earthquakes" title="Earthquakes">Earthquakes</a></li>
+                    <li><a href="/olympics" title="Olympics">Olympics</a></li>
+                    <li><a href="/other-sports" title="Other Sports">Other Sports</a></li>
+                    <li><a href="/scoreboard" title="Other Sports">Scores and Stats</a></li>
+                    <li><a href="/sports-headlines" title="Sports Headlines">Sports Headlines</a></li>
+                    <li><a href="/snow-reports " title="Snow Reports">Snow Reports</a></li>
+                </ul>
+            </li>
+
+            <li class="nav-header">Opinion
+                <ul class="list">
+                    <li><a href="/opinion" title="Opinion"><strong>All Opinion</strong><a></li>
+                    <li><a href="/letters" title="Perspective">Letters to the Editor</a></li>
+                    <li><a href="/columns" title="Columns">Columns</a>
+                        <ul class="circle">
+                            <li><a href="/bizbreak" title="Business Break">Business Break</a></li>
+                            <li><a href="/mike-cassidy" title="Mike Cassidy">Mike Cassidy</a></li>
+                            <li><a href="/laurie-daniel" title="Laurie Danie">Laurie Daniel</a></li>
+                            <li><a href="/karen-dsouza" title="Karen d'Souza">Karen d'Souza</a></li>
+                            <li><a href="/scott-herhold" title="Scott Herhold">Scott Herhold</a></li>
+                            <li><a href="/john-horgan" title="John Horgan">John Horgan</a></li>
+                            <li><a href="/internal-affairs" title="Internal Affairs">Internal Affairs</a></li>
+                            <li><a href="/tim-kawakami" title="Tim Kawakami">Tim Kawakami</a></li>
+                            <li><a href="/carolyn-livengood" title="Carolyn Livengood">Carolyn Livengood</a></li>
+                            <li><a href="/sal-pizarro" title="Sal Pizarro">Sal Pizarro</a></li>
+                            <li><a href="/mark-purdy" title="Mark Purdy">Mark Purdy</a></li>
+                            <li><a href="/mr-roadshow" title="Mr. Roadshow (Gary Richards)">Mr. Roadshow (Gary Richards)</a></li>
+                            <li><a href="/poker" title="Poker">
+                            Steve Rosenbloom on Poker</a></li>
+                            <li><a href="/tech-notebook" title="Tech Notebook">Tech Notebook</a></li>
+                            <li><a href="/steve-yvaska" title="Steve Yvaska">Steve Yvaska</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="/blogs" title="Blogs">Blogs</a></li>
+                    <li><a href="http://forums.mercurynews.com" title="Discussion Boards">Discussion Boards</a></li>
+                    <li><a href="http://forums.mercurynews.com/forums/forum/673" title="Article Comments">Article Comments</a></li>
+                    <li><a href="http://forums.mercurynews.com/forums/poll" title="Web Votes">Web Votes</a></li>
+                    <li><a href="/live-chats" title="Live Chats">Live Chats</a></li>
+                </ul>
+            </li>
+
+            <li class="nav-header">Entertainment
+                <ul class="list">
+                    <li><a href="/celebrities" title="Celebrities">Celebrities</a></li>
+                    <li><a href="/restaurants" title="Restaurants">Restaurants</a></li>
+                    <li><a href="/movies-dvd" title="Movies">Movies</a></li>
+                    <li><a href="/television" title="Television">Television</a></li>
+                    <li><a href="/music" title="Music">Music</a></li>
+                    <li><a href="/horoscopes" title="Horoscopes">Horoscopes</a></li>
+                    <li><a href="/comics" title="Comics">Comics</a></li>
+                    <li><a href="/puzzles-games" title="Puzzles / Games">Puzzles / Games</a></li>
+                    <li><a href="http://events.mercurynews.com" title="Events">Events</a></li>
+                    <li><a href="/theater-museums" title="Theater / Museums">Theater / Museums</a></li>
+                    <li><a href="/books" title="Books">Books</a></li>
+                    <li><a href="/video-games" title="Video Games">Video Games</a></li>
+                    <li><a href="/entertainment-headlines" title="Entertainment Headlines">Entertainment Headlines</a></li>
+                </ul>
+            </li>
+
+            <li class="nav-header">Bay Area Living
+                <ul class="list">
+                    <li><a href="/family-relationships" title="Family">Family and Relationships</a></li>
+                    <li><a href="/fashion-style" title="Fashion &amp; Style">Fashion &amp; Style</a></li>
+                    <li><a href="/food-wine" title="Food &amp; Wine">Food &amp; Wine</a>
+                        <ul class="circle">
+                            <li><a href="/recipes" title="Recipes">Recipes</a></li>
+                            <li><a href="/south-bay-wine" title="South Bay Wine">South Bay Uncorked</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="/holidays" title="Holidays">Holidays</a></li>
+                    <li><a href="/home-garden" title="Home and Garden">Home and Garden</a></li>
+                    <li><a href="/our-say" title="Our Say">Our Say</a></li>
+                    <li><a href="/travel" title="Travel">Travel</a>
+                        <ul class="circle">
+                            <li><a href="/san-francisco-bay-area-travel" title="Bay Area Travel">Bay Area Travel</a></li>
+                            <li><a href="/reno-tahoe" title="Reno / Tahoe">Reno / Tahoe</a></li>
+                            <li><a href="/wine-region" title="Wine Region">Wine Region</a></li>
+                            <li><a href="/santa-cruz-travel" title="Santa Cruz">Santa Cruz</a></li>
+                            <li><a href="/monterey" title="Monterey">Monterey</a></li>
+                            <li><a href="/mexico" title="Mexico">Mexico</a></li>
+                            <li><a href="/las-vegas" title="Las Vegas">Las Vegas</a></li>
+                            <li><a href="/hawaii" title="Hawaii">Hawaii</a></li>
+                            <li><a href="/southern-california-travel" title="Southern California">Southern California</a></li>
+                            <li><a href="/asia-china-india-vietnam-taiwan" title="Asia">Asia</a></li>
+                            <li><a href="/travel-europe" title="Europe">Europe</a></li>
+                            <li><a href="/ski-snow" title="Ski-Snow">Ski-Snow</a></li>
+                            <li><a href="/cruises" title="Cruises">Cruises</a></li>
+                            <li><a href="/theme-parks" title="Theme Parks">Theme Parks</a></li>
+                            <li><a href="/travel-deals-more" title="Deals and Specials">Deals and Specials</a></li>
+                            <li><a href="/travel-web" title="Around the Web">Around the Web</a></li>
+                            <li><a href="/travel-gadgets" title="Gadgets to Go">Gadgets to Go</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="/pets-animals" title="Pets &amp; Animals">Pets &amp; Animals</a></li>
+                    <li><a href="/green-living" title="Green Living">Green Living</a></li>
+                    <li><a href="/teens" title="Teens">Teens</a></li>
+                    <li><a href="/religion" title="Religion">Religion</a></li>
+                </ul>
+            </li>
+
+            <li class="nav-header">My Town
+                <ul class="list">
+                    <li><a href="/san-jose-neighborhoods" title="San Jose Neighborhoods">San Jose Neighborhoods</a></li>
+                    <li><a href="/los-gatos" title="Los Gatos">Los Gatos</a></li>
+                    <li><a href="/saratoga" title="Saratoga">Saratoga</a></li>
+                    <li><a href="/cupertino" title="Cupertino">Cupertino</a></li>
+                    <li><a href="/sunnyvale" title="Sunnyvale">Sunnyvale</a></li>
+                    <li><a href="/campbell" title="Campbell">Campbell</a></li>
+                    <li><a href="/peninsula" title="Peninsula">Peninsula</a></li>
+                    <li><a href="/central-coast" title="Central Coast">Central Coast</a></li>
+                    <li><a href="/pacifica" title="Pacifica">Pacifica</a></li>
+                    <li><a href="/san-mateo-county" title="San Mateo County">San Mateo County</a></li>
+                    <li><a href="/milpitas" title="Milpitas">Milpitas</a></li>
+                    <li><a href="/fremont" title="Fremont">Fremont</a></li>
+                    <li><a href="/alameda-county" title="Alameda County">Alameda County</a></li>
+                </ul>
+            </li>
+        </ul>
+        <div class="bottom-items">
+            <div class="col md-4">
+                <ul class="expanded">
+                    <li class="nav-header">Site Services</li>
+                    <li>
+                        <ul class="list">
+                            <li><a href="http://m.mercurynews.com/cb/473/subscribe.html" title="Mobile Edition">Mobile Edition / Alerts</a></li>
+                            <li><a href="https://secure.passport.mnginteractive.com/mngi/servletDispatch/ErightsPassportServlet.dyn?url=http://www.mercurynews.com" title="Newsletters">Email Newsletters</a></li>
+                            <li><a href="/twitter" title="Twitter">Twitter</a></li>
+                            <li><a href="/rss" title="RSS">RSS Feeds</a></li>
+                            <li><a href="/widgets" title="Widgets">Widgets</a></li>
+                            <li><a href="https://my.mercurynews.com/authenticate.asp" title="Home Delivery">Home Delivery</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div class="col md-4">
+                <ul class="expanded">
+                    <li class="nav-header">Help</li>
+                    <li>
+                        <ul class="list">
+                            <li><a href="/contact-us" title="Contact Us">Contact Us</a></li>
+                            <li><a href="/faq" title="FAQ">FAQ</a></li>
+                            <li><a href="/archive-search" title="Archives">Archives</a></li>
+                            <li><a href="/contact-us" title="Contact Us">Contact Us</a></li>
+                            <li><a href="http://www.mercurynews2.com/services" title="Start / Stop Paper">Start / Stop Paper Us</a></li>
+                            <li><a href="/advertise" title="Advertising">Advertising</a></li>
+                            <li><a href="http://buyphotos.mercurynews.com" title="Buy Photos">Buy Photos</a></li>
+                            <li> <a href="/ethics-policy" title="Ethics Policy">Ethics Policy</a> </li>
+                            <li><a href="/copyright" title="Copyright">Copyright</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div class="col md-4">
+                <ul class="expanded">
+                    <li class="nav-header">Marketplace</li>
+                    <li>
+                        <ul class="list">
+                            <li><a href="">Autos</a></li>
+                            <li><a href="">Classifieds</a></li>
+                            <li><a href="">Real Estate</a></li>
+                            <li><a href="">Jobs</a></li>
+                            <li><a href="">Shopping</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+
+
+        </div>
+
+
+    </div> <!-- .content-well -->
+    <div class="right-rail">
+        <?php include('temps/right-rail.php'); ?>
+    </div> <!-- .right-rail -->
+</div> <!-- .row -->
+<div class="row">
+    <div class="col md-12">
+        <?php include('temps/content/bottom-line.php'); ?>
+    </div>
+</div>
+<?php require_once('temps/footer.php'); // load in footer ?>
