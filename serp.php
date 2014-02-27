8f5edfb752618daabf16f613f45cf490d000311f
jboho <bohoj@mac.com>
2014-02-27 16:18:41 -0700

    Fixes issue #8 -- search results page is in

---
+++ serp.php
@@ -0,0 +1,174 @@
+<?php require_once('inc/init.php'); // load in base objects ?>
+<?php
+    $page_type = 'section';
+    $master_name = 'search results';
+    $sub_name = 'search results';
+?>
+<?php require_once('temps/header.php'); // load in header ?>
+<div class="row serp">
+    <div class="content-well">
+        <h1 class="page-header">Search results for search term here <small>8,212 results</small></h1>
+        <div class="row">
+            <div class="results col md-8 md-push-4">
+                <div class="well">
+                    <form id="site-search-serp" action="#" method="get" role="search">
+                        <label for="">Search for:</label>
+                        <div class="input-group">
+                            <input id="search-field" type="text" name="s" class="form-control input-lg" placeholder="search term here">
+                            <span class="input-group-btn">
+                                <button id="search-button" class="btn primary lg" type="button"><span class="glyphicon glyphicon-search"><span class="sr-only">Search</span></span></button>
+                            </span>
+                        </div><!-- /input-group -->
+                    </form> <!-- #site-search -->
+                </div>
+                <ul>
+                    <li>
+                        <a href="" class="headline">Headline goes in this space</a>
+                        <p class="byline">By some reporter, souce goes here</p>
+                        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <p class="meta">2014-2-27 10:51:13 A.M. PST</p>
+                    </li>
+                    <li>
+                        <a href="" class="headline">Headline goes in this space</a>
+                        <p class="byline">By some reporter, souce goes here</p>
+                        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <p class="meta">2014-2-27 10:51:13 A.M. PST</p>
+                    </li>
+                    <li>
+                        <a href="" class="headline">Headline goes in this space</a>
+                        <p class="byline">By some reporter, souce goes here</p>
+                        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <p class="meta">2014-2-27 10:51:13 A.M. PST</p>
+                    </li>
+                    <li>
+                        <a href="" class="headline">Headline goes in this space</a>
+                        <p class="byline">By some reporter, souce goes here</p>
+                        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <p class="meta">2014-2-27 10:51:13 A.M. PST</p>
+                    </li>
+                    <li>
+                        <a href="" class="headline">Headline goes in this space</a>
+                        <p class="byline">By some reporter, souce goes here</p>
+                        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <p class="meta">2014-2-27 10:51:13 A.M. PST</p>
+                    </li>
+                    <li>
+                        <a href="" class="headline">Headline goes in this space</a>
+                        <p class="byline">By some reporter, souce goes here</p>
+                        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <p class="meta">2014-2-27 10:51:13 A.M. PST</p>
+                    </li>
+                    <li>
+                        <a href="" class="headline">Headline goes in this space</a>
+                        <p class="byline">By some reporter, souce goes here</p>
+                        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <p class="meta">2014-2-27 10:51:13 A.M. PST</p>
+                    </li>
+                    <li>
+                        <a href="" class="headline">Headline goes in this space</a>
+                        <p class="byline">By some reporter, souce goes here</p>
+                        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <p class="meta">2014-2-27 10:51:13 A.M. PST</p>
+                    </li>
+                    <li>
+                        <a href="" class="headline">Headline goes in this space</a>
+                        <p class="byline">By some reporter, souce goes here</p>
+                        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <p class="meta">2014-2-27 10:51:13 A.M. PST</p>
+                    </li>
+                    <li>
+                        <a href="" class="headline">Headline goes in this space</a>
+                        <p class="byline">By some reporter, souce goes here</p>
+                        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <p class="meta">2014-2-27 10:51:13 A.M. PST</p>
+                    </li>
+                    <li>
+                        <a href="" class="headline">Headline goes in this space</a>
+                        <p class="byline">By some reporter, souce goes here</p>
+                        <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <p class="meta">2014-2-27 10:51:13 A.M. PST</p>
+                    </li>
+                </ul>
+                <ul class="pagination">
+                  <li><a href="#">&laquo;</a></li>
+                  <li><a href="#">1</a></li>
+                  <li><a href="#">2</a></li>
+                  <li><a href="#">3</a></li>
+                  <li><a href="#">4</a></li>
+                  <li><a href="#">5</a></li>
+                  <li><a href="#">&raquo;</a></li>
+                </ul>
+            </div>
+			<div class="filters col md-4 md-pull-8 visible-md-up">
+				<div class="panel primary">
+					<div class="panel-heading">
+						<h4 class="panel-title">Refine your search</h4>
+					</div>
+					<div class="panel-body">
+						<div class="filter">
+							<p class="page-header h5">Sort by</p>
+							<div class="radio">
+							  <label>
+							    <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+							    Relevance
+							  </label>
+							</div>
+							<div class="radio">
+								<label>
+									<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+									Most recent
+								</label>
+							</div>
+							<div class="radio">
+								<label>
+									<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+									Least recent
+								</label>
+							</div>
+							<div class="radio">
+								<label>
+									<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+									Author A-Z
+								</label>
+							</div>
+							<div class="radio">
+								<label>
+									<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+									Author Z-A
+								</label>
+							</div>
+						</div> <!-- .filter -->
+						<div class="filter">
+							<p class="page-header h5">Narrow by</p>
+							<p class="h6">Date:</p>
+							<ul class="nav pills stacked">
+								<li class="active"><a href="">All</a></li>
+								<li><a href="">Today</a></li>
+								<li><a href="">Less than a month old</a></li>
+								<li><a href="">Less than 3 months old</a></li>
+								<li><a href="">Less than a year old</a></li>
+							</ul>
+							<p class="h6">Category:</p>
+							<ul class="nav pills stacked">
+								<li class="active"><a href="">All <span class="badge">1,123</span></a></li>
+								<li><a href="">Sports <span class="badge">650</span></a></li>
+								<li><a href="">News <span class="badge">554</span></a></li>
+								<li><a href="">Football <span class="badge">422</span></a></li>
+								<li><a href="">Broncos <span class="badge">322</span></a></li>
+								<li><a href="">State <span class="badge">111</span></a></li>
+								<li><a href="">Local <span class="badge">42</span></a></li>
+								<li><a href="">48 more <span class="caret"></span></a></li>
+							</ul>
+						</div> <!-- .filter -->
+					</div> <!-- .panel-body -->
+				</div> <!-- .panel -->
+			</div> <!-- .col -->
+		</div>
+	</div> <!-- .content-well -->
+    <div class="right-rail">
+        <?php include('temps/right-rail.php'); ?>
+    </div> <!-- .right-rail -->
+</div>
+
+<?php include('temps/content/bottom-line.php'); ?>
+<?php require_once('temps/footer.php'); // load in footer ?>
