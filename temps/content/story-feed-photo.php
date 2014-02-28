924a1aee397f0985ce42f3932fbea48b955e13a8
jboho <bohoj@mac.com>
2014-02-27 18:53:43 -0700

    Tweaks to columnists thing

--- temps/content/story-feed-photo.php
+++ temps/content/story-feed-photo.php
@@ -1,24 +1,20 @@
 <div id="columnists" class="story-feed sm">
     <h4 class="topic-header">Opinion</h4>
-    <figure>
-        <a href=""><img src="http://placehold.it/400x300" alt=""></a>
-        <hgroup>
-            <h5><a href="">Joanne Davidson</a></h5>
+    <ul>
+        <li>
+            <a href=""><img src="http://placehold.it/400x300" alt=""></a>
+            <a class="h5" href="">Joanne Davidson</a>
             <h3><a href="">Colorado gives, and it just keeps giving</a></h3>
-        </hgroup>
-    </figure>
-    <figure>
-        <a href=""><img src="http://placehold.it/400x300" alt=""></a>
-        <hgroup>
-            <h5><a href="">Vincent Carroll</a></h5>
+        </li>
+        <li>
+            <a href=""><img src="http://placehold.it/400x300" alt=""></a>
+            <a class="h5" href="">Vincent Carroll</a>
             <h3><a href="">Am I cynical over the pit bull loophole? Yes.</a></h3>
-        </hgroup>
-    </figure>
-    <figure>
-        <a href=""><img src="http://placehold.it/400x300" alt=""></a>
-        <hgroup>
-            <h5><a href="">Woody Paige</a></h5>
+        </li>
+        <li>
+            <a href=""><img src="http://placehold.it/400x300" alt=""></a>
+            <a class="h5" href="">Woody Paige</a>
             <h3><a href="">Read between the lines, folks. It's Tebow time.</a></h3>
-        </hgroup>
-    </figure>
+        </li>
+    </ul>
 </div>
