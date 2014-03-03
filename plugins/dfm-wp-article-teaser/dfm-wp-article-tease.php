55db1f202d55376a3be6f763a64040544fb02479
Joe Murphy <joe.murphy@gmail.com>
2014-03-03 12:50:31 -0700

    Write the custom field

--- plugins/dfm-wp-article-teaser/dfm-wp-article-tease.php
+++ plugins/dfm-wp-article-teaser/dfm-wp-article-tease.php
@@ -14,10 +14,52 @@
 // Usage:
 */
 
-if ( !file_exists(WP_PLUGIN_DIR . '') ) die("Requires plugin (  )");
+if ( !file_exists(WP_PLUGIN_DIR . '/easy-custom-fields/easy-custom-fields.php') ) die("Requires Easy Custom Fields plugin ( http://wordpress.org/plugins/easy-custom-fields/ )");
 
-require_once( WP_PLUGIN_DIR . '' );
+require_once( WP_PLUGIN_DIR . '/easy-custom-fields/easy-custom-fields.php' );
 
+$field_data = array (
+        'Teaser' => array (
+                'fields' => array (
+                        'package' => array('label'=>'Teaser',
+                                        'type'=>'link_select'),
+                ),
+        ),
+);
+
+if ( !class_exists( "Easy_CF_Field_Link_Select" ) ) {
+    class Easy_CF_Field_Link_Select extends Easy_CF_Field {
+        public function print_form() {
+            $class = ( empty( $this->_field_data['class'] ) ) ? $this->_field_data['id'] . '_class' :  $this->_field_data['class'];
+            $input_class = ( empty( $this->_field_data['input_class'] ) ) ? $this->_field_data['id'] . '_input_class' :  $this->_field_data['input_class'];
+
+            $id = ( empty( $this->_field_data['id'] ) ) ? $this->_field_data['id'] :  $this->_field_data['id'];
+            $label = ( empty( $this->_field_data['label'] ) ) ? $this->_field_data['id'] :  $this->_field_data['label'];
+            $value = $this->get();
+
+            //$items = get_tags();
+            $options = '<option value=""></option>';
+            $selected = '';
+
+            foreach ( $items as $item ):
+                if ( $tag->slug == $value ) $selected = 'selected';
+                $options .= '<option value="' . $tag->slug . '" . ' . $selected . '>' . $tag->name . '</option>' . "\n";
+                $selected = '';
+            endforeach;
+
+            $hint = ( empty( $this->_field_data['hint'] ) ) ? '' :  '<p><em>' . $this->_field_data['hint'] . '</em></p>';
+
+            $label_format =
+                '<div class="%s">'.
+                '<p><label for="%s"><strong>%s</strong></label></p>'.
+                '<p><select class="%s" style="width: 100%%;" name="%s">%s</select></p>'.
+                '%s'.
+                '</div>';
+            printf( $label_format, $class, $id, $label, $input_class, $id, $options, $hint );
+        }
+    }
+}
+$easy_cf = new Easy_CF($field_data);
 
 class DFMInArticleTeaser
 {
