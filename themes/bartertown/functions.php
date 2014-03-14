fc90704de1d5c933f63e15726aadb0a4692ab4f9
kozlowskij <joshuamkozlowski@gmail.com>
2014-03-14 08:33:51 -1000

    testing some other curation positions

--- themes/bartertown/functions.php
+++ themes/bartertown/functions.php
@@ -360,9 +360,25 @@ if (class_exists('Fieldmanager_Group')) {
         // Account for centerpiece on all section fronts by default (if there's no json config for it)?
         $fm = new Fieldmanager_Checkbox('Click here if you want this post to show as the centerpiece', array(
             'name' => 'lead_story',
-            'checked_value' => 'no'
+            'checked_value' => 'yes' // custom field values are always strings, no booleans?
         ));
         $fm->add_meta_box('Centerpiece', array('post'));
+
+        // Secondary lead story
+        $fm = new Fieldmanager_Checkbox('Click here if you want this post to show 
+            as a secondary lead story', array(
+            'name' => 'secondary_lead_story',
+            'checked_value' => 'yes'
+        ));
+        $fm->add_meta_box('Secondary lead story', array('post'));
+        
+        // Secondary stories
+        $fm = new Fieldmanager_Checkbox('Click here if you want this post to show 
+            as a secondary story for the relevant category', array(
+            'name' => 'secondary_story',
+            'checked_value' => 'yes'
+        ));
+        $fm->add_meta_box('Secondary story', array('post'));
     });
 
 }
