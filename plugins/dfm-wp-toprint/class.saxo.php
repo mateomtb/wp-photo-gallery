97d312c22880ee0588d8b26be92c6698201ca161
Joe Murphy <joe.murphy@gmail.com>
2014-04-02 13:52:13 -0600

    Added skeleton file for saxo-specific code

---
+++ plugins/dfm-wp-toprint/class.saxo.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Extends DFP To Print plugin functionality for the Saxotech publishing system.
+
+
+*/
+
+
+class SaxoArticle extends DFMToPrintArticle
+{
+
+}
+
+class SaxoUser extend DFMToPrintUser
+{
+
+}
