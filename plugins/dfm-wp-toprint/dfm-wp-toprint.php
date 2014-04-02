abeaecda62b76030173055a7f8ee2aff82b93bb2
Joe Murphy <joe.murphy@gmail.com>
2014-04-02 14:10:24 -0600

    More cleanup

--- plugins/dfm-wp-toprint/dfm-wp-toprint.php
+++ plugins/dfm-wp-toprint/dfm-wp-toprint.php
@@ -99,7 +99,7 @@ class DFMRequest
 
     private function get_credentials()
     {
-        // Return a "user:password" formatted credentials for saxo.
+        // Return a "user:password" formatted credentials.
         return file_get_contents('.credentials');
     }
 
@@ -154,15 +154,3 @@ class DFMRequest
 }
 
 
-function send_to_saxo($post_id)
-{
-    $article = new DFMSaxoArticle($post_id);
-    $xml = $article->get_article();
-    // *** initiate curl
-    // *** if this is an article update, get the saxo article id (custom field)
-    // *** send document to saxo
-    // *** if this is a new article, get the saxo article id and store it in a custom field
-    // *** Get the response from saxo, let the user know if it failed or succeeded.
-    // *** If it failed, print as many relevant details of the failure as possible.
-}
-add_action( 'save_post', 'send_to_saxo' );
